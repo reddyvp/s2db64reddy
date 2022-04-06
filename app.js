0a505c904ec7b4ebf13ab248038350127dd82dd5
reddyvp <reddyvamsidharp@gmail.com>
2022-04-06 18:07:28 -0500

    addmods created

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var friendRouter = require('./routes/friend');
+var addmodsRouter = require('./routes/addmods');
 var app = express();
 
 // view engine setup
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/friend',friendRouter);
-
+app.use('/addmods',addmodsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
