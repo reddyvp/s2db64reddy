489883e3b25d98a6588a31ab0c07a5e27779711f
reddyvp <reddyvamsidharp@gmail.com>
2022-04-06 15:47:56 -0500

    bug resolved

--- routes/friend.js
+++ routes/friend.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('friend', { title: 'Search Result friend' });
+  res.render('friend', { title: 'dhbfq' });
 });
 
 module.exports = router;
