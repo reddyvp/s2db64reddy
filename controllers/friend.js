a4f9b51c61b5b3ce50b6da1ba65afed276bf5948
reddyvp <reddyvamsidharp@gmail.com>
2022-04-20 20:40:28 -0500

    All files added

--- controllers/friend.js
+++ controllers/friend.js
@@ -1,55 +1,163 @@
-var friend = require('../models/friend'); 
- 
+var friend = require('../models/friend');
+
 // List of all friends 
 // VIEWS 
 // Handle a show all view 
-exports.friend_view_all_Page = async function(req, res) { 
-    try{ 
-        thefriends = await friend.find(); 
+exports.friend_view_all_Page = async function (req, res) {
+    try {
+        thefriends = await friend.find();
+        console.log(thefriends)
+        res.send(thefriends)
+        // res.render('friend', { title: 'Friend Search Results', results: thefriends }); 
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+// Handle a show all view 
+exports.friends_view_all_Page = async function (req, res) {
+    try {
+        thefriends = await friend.find();
         console.log(thefriends)
         // res.send(thefriends)
-        res.render('friend', { title: 'Friend Search Results', results: thefriends }); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
- 
- 
+        res.render('friend', { title: 'Friend Search Results', results: thefriends });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+
 // for a specific friend. 
-exports.friend_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: friend detail: ' + req.params.id); 
-}; 
- 
-// Handle Costume create on POST. 
-exports.friend_create_post = async function(req, res) { 
-    console.log(req.body) 
-    let document = new friend(); 
+exports.friend_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: friend detail: ' + req.params.id);
+};
+
+// Handle friend create on POST. 
+exports.friend_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new friend();
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
-    document.name = req.body.name; 
-    document.age = req.body.age; 
-    document.height = req.body.height; 
+    // {"friend_type":"goat", "cost":12, "size":"large"} 
+    document.name = req.body.name;
+    document.age = req.body.age;
+    document.height = req.body.height;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// for a specific friend. --lab
+exports.friend_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await friend.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+
+// Handle friend delete form on DELETE. 
+exports.friend_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: friend delete DELETE ' + req.params.id);
+};
+
+// Handle friend update form on PUT. 
+exports.friend_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await friend.findById(req.params.id)
+        // Do updates of properties 
+        if (req.body.name)
+            toUpdate.name = req.body.name;
+        if (req.body.age) toUpdate.age = req.body.age;
+        if (req.body.height) toUpdate.height = req.body.height;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`);
+    }
+};
+// Handle friend delete on DELETE. 
+exports.friend_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await friend.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
+// Handle a show one view with id specified by query 
+exports.friend_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await friend.findById(req.query.id)
+        res.render('frienddetail',
+            { title: 'Friend Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+    // Handle building the view for creating a friend. 
+    // No body, no in path parameter, no query. 
+    // Does not need to be async 
+    exports.friend_create_Page = function (req, res) {
+        console.log("create view")
+        try {
+            res.render('friendcreate', { title: 'friend Create' });
+        }
+        catch (err) {
+            res.status(500)
+            res.send(`{'error': '${err}'}`);
+        }
+    };
+
+    // Handle building the view for updating a friend. 
+// query provides the id 
+exports.friend_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
     try{ 
-        let result = await document.save(); 
-        res.send(result); 
+        let result = await friend.findById(req.query.id) 
+        res.render('friendupdate', { title: 'friend Update', toShow: result }); 
     } 
     catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
- 
- 
-// Handle friend delete form on DELETE. 
-exports.friend_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: friend delete DELETE ' + req.params.id); 
-}; 
- 
-// Handle friend update form on PUT. 
-exports.friend_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: friend update PUT' + req.params.id); 
+
+//Handle a delete one view with id from query 
+exports.friend_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await friend.findById(req.query.id) 
+        res.render('frienddelete', { title: 'friend Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
--- routes/friend.js
+++ routes/friend.js
@@ -7,6 +7,22 @@ const frndController = require('../controllers/friend')
 //   res.render('friend', { title: 'Search Results Friend' });
 // });
 /* GET costumes */ 
-router.get('/', frndController.friend_view_all_Page ); 
+router.get('/', frndController.friends_view_all_Page ); 
+
+// GET request for one costume. --lab
+router.get('/friend/:name', frndController.friend_detail);
+
+/* GET detail costume page */ 
+router.get('/detail', frndController.friend_view_one_Page); 
+
+/* GET create costume page */ 
+router.get('/create', frndController.friend_create_Page);
+
+/* GET create update page */ 
+router.get('/update', frndController.friend_update_Page); 
+
+/* GET create update page */ 
+router.get('/delete', frndController.friend_delete_Page); 
+
 
 module.exports = router;
